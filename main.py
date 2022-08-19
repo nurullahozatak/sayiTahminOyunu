7a30bf7a1a9a7e83c15e7e1202a272f995da4cc2
NurullahÖzatak <nrllh.319@gmail.com>
2022-08-19 23:24:02 +0300

    .

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import random
 
-number = random.randint(1, 100)
-point = 100
-correction = True
 
-print("*************** Sayı Tahmin Oyunu *************** \n")
-print("Toplam 100 puanınız var ve sayıyı tahmin etmek için 25 hakkınız bulunmakta\n")
-while correction:
-    guess = int(input("Sayı tahmin edin : "))
-    if number < guess:
-        print("Aşağı")
-        point -= 5
-    elif number > guess:
-        print("Yukarı")
-        point -= 5
-    else:
-        print("Kazandınız Doğru Tahmin!")
-        print("Puanınız :" ,point)
-        correction = False
+def query():
+    number = random.randint(1, 100)
+    point = 100
+    correction = True
 
+    print("*************** Sayı Tahmin Oyunu *************** \n")
+    print("Toplam 100 puanınız var ve sayıyı tahmin etmek için 25 hakkınız bulunmakta\n")
+    while correction:
+        guess = int(input("Sayı tahmin edin : "))
+        if number < guess:
+            print("Aşağı")
+            point -= 5
+        elif number > guess:
+            print("Yukarı")
+            point -= 5
+        else:
+            print("Kazandınız Doğru Tahmin!")
+            print("Puanınız :", point)
+            correction = False
 
+query()
+print("Tekrar oynamak ister misiniz ?")
+answer = input("Answer (Y/N)")
+if answer == "N":
+    correction = False
+elif answer == "Y":
+    query()
