04eaee6c53a42000cc6e38911e76b2726fe081ce
NurullahÖzatak <nrllh.319@gmail.com>
2022-08-19 23:33:15 +0300

    .

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import random
+correction = True
+while correction:
+    def query():
+        global correction
+        number = random.randint(1, 100)
+        point = 100
+
+        print("*************** Sayı Tahmin Oyunu *************** \n")
+        print("Toplam 100 puanınız var ve sayıyı tahmin etmek için 25 hakkınız bulunmakta\n")
+        while correction:
+            guess = int(input("Sayı tahmin edin : "))
+            if number < guess:
+                print("Aşağı")
+                point -= 5
+            elif number > guess:
+                print("Yukarı")
+                point -= 5
+            else:
+                print("Kazandınız Doğru Tahmin!")
+                print("Puanınız :", point)
+                correction = False
 
 
-def query():
-    number = random.randint(1, 100)
-    point = 100
-    correction = True
-
-    print("*************** Sayı Tahmin Oyunu *************** \n")
-    print("Toplam 100 puanınız var ve sayıyı tahmin etmek için 25 hakkınız bulunmakta\n")
-    while correction:
-        guess = int(input("Sayı tahmin edin : "))
-        if number < guess:
-            print("Aşağı")
-            point -= 5
-        elif number > guess:
-            print("Yukarı")
-            point -= 5
-        else:
-            print("Kazandınız Doğru Tahmin!")
-            print("Puanınız :", point)
-            correction = False
-
-query()
-print("Tekrar oynamak ister misiniz ?")
-answer = input("Answer (Y/N)")
-if answer == "N":
-    correction = False
-elif answer == "Y":
     query()
+    print("Tekrar oynamak ister misiniz ?")
+    answer = input("Answer (Y/N)")
+    if answer == "N":
+        correction = False
+    elif answer == "Y":
+        query()
+
